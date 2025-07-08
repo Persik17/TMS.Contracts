8bd3ec38d0259d1ed97bbdbf84bfed6c42936b91
Aleksandr <kraichik03@mail.ru>
2025-07-08 12:41:45 +0300

    fix: update event fields

--- TMS.Contracts/Events/UserInvitationCreatedEvent.cs
+++ TMS.Contracts/Events/UserInvitationCreatedEvent.cs
@@ -7,11 +7,12 @@ namespace TMS.Contracts.Events
     public class UserInvitationCreatedEvent : IEvent
     {
         public Guid InvitationId { get; set; }
+        public Guid UserId { get; set; }
         public string Email { get; set; }
-        public string FullName { get; set; }
-        public string[] Roles { get; set; }
-        public string Language { get; set; }
-        public string Message { get; set; }
+        public string? FullName { get; set; }
+        public string[]? Roles { get; set; }
+        public string? Language { get; set; }
+        public string? Message { get; set; }
         public Guid InvitedBy { get; set; }
         public DateTime Expiration { get; set; }
     }
