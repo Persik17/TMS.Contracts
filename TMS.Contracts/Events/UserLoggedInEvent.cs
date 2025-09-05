300f73d65077565467f05124af245f226188d78c
nagithube <agalarovgera@mail.ru>
2025-09-05 13:52:11 +0300

    добавил новое событие

---
+++ TMS.Contracts/Events/UserLoggedInEvent.cs
@@ -0,0 +1,10 @@
+﻿using MassTransit;
+using TMS.Contracts.Models;
+
+namespace TMS.Contracts.Events;
+
+[EntityName("user_logged_in")]
+public class UserLoggedInEvent : IEvent
+{
+    public Guid UserId { get; set; }
+}
